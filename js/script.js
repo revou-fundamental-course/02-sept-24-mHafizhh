9e615f94681faeb31fa6607622c917f36c449d57
Muhammad Hafizh <mhafizh2409@gmail.com>
2024-09-12 11:09:18 +0700

    Perhitungan Segitiga dan Jajar Genjang

--- js/script.js
+++ js/script.js
@@ -1,78 +1,114 @@
+// Luas Segitiga
 function hitungLuasSegitiga() {
-    var alas = document.getElementById("panjang-alas").value;
-    var tinggi = document.getElementById("tinggi").value;
-    var hasilLuas = document.getElementById("hasil-luas");
+    let alas = document.getElementById("alas-segitiga").value;
+    let tinggi = document.getElementById("tinggi-segitiga").value;
+    let hasilLuasSegitiga = document.getElementById("hasil-Luas"); 
+    let rumusluas = document.getElementById("rumus-luas-segitiga");
 
     if (alas > 0 && tinggi > 0) {
-        var luas = 0.5 * alas * tinggi;
-        hasilLuas.textContent = "Luas Segitiga adalah: " + luas;
+        let luas = 0.5 * alas * tinggi;
+        rumusluas.textContent = "L = 1/2 (a x t)";
+        hasilLuasSegitiga.innerHTML = "L = 1/2 ("+ alas +" x "+ tinggi +") <br> "+ "L = "+ luas;
     } else {
-        hasilLuas.textContent = "Masukkan Nilai Panjang Alas dan Tinggi Terlebih Dahulu";
+        alert('Mohon Isi Form Terlebih Dahulu');
     }
 }
 
+//Keliling Segitiga
 function hitungKelilingSegitiga() {
-    var sisi1 = parseFloat(document.getElementById("panjang-sisi1").value);
-    var sisi2 = parseFloat(document.getElementById("panjang-sisi2").value);
-    var sisi3 = parseFloat(document.getElementById("panjang-sisi3").value);
-    var hasilKeliling = document.getElementById("hasil-keliling");
+    let sisi1 = parseFloat(document.getElementById("sisi1").value);
+    let sisi2 = parseFloat(document.getElementById("sisi2").value);
+    let sisi3 = parseFloat(document.getElementById("sisi3").value);
+    let hasilKeliling = document.getElementById("hasilKelilingSegitiga");
+    let rumusKeliling = document.getElementById("rumus-keliling-segitiga");
 
     if (sisi1 > 0 && sisi2 > 0 && sisi3 >0) {
-        var keliling = sisi1 + sisi2 + sisi3;
-        hasilKeliling.textContent = "Keliling Segitiga adalah: " + keliling;
+        let keliling = sisi1 + sisi2 + sisi3;
+        rumusKeliling.textContent = "K = S + S + S";
+        hasilKeliling.innerHTML = "K = "+ sisi1 + "+"+ sisi2+ "+"+ sisi3 + "<br> K = " + keliling;
     } else {
-        hasilKeliling.textContent = "Masukkan nilai yang valid untuk panjang sisi.";
+        alert('Mohon Isi Form Terlebih Dahulu');
     }
 }
 
-function resetLuasSegitiga() {
-    document.getElementById("panjang-alas").value = "";
-    document.getElementById("tinggi").value = "";
-    document.getElementById("hasil-luas").textContent = "";
+//---------------------------------------------------Rumus Jajar Genjang
+//Luas Jajar Genjang
+function hitungLuasJajargenjang(){
+    let alas = parseFloat(document.getElementById("alas-jajargenjang").value);
+    let tinggi = parseFloat(document.getElementById("tinggi-jajargenjang").value);
+    let hasilLuas = document.getElementById("hasil-luas-jajargenjang");
+    let rumusluas =  document.getElementById("rumus-luas-jajargenjang");
+
+    if (alas > 0 && tinggi > 0) {
+        let luas = alas * tinggi;
+        rumusluas.textContent = "L = a x t";
+        hasilLuas.innerHTML = "L = "+ alas +" x " + tinggi + "<br>L = " + luas;
+    } else {
+        alert('Mohon Isi Form Terlebih Dahulu');
+    }
+}
+
+//Keliling Jajar Genjang
+function hitungKelilingJajargenjang(){
+    let a = parseFloat(document.getElementById("a-jajargenjang").value);
+    let b = parseFloat(document.getElementById("b-jajargenjang").value);
+    let hasilkeliling = document.getElementById("hasil-keliling-jajargenjang");
+    let rumuskeliling =  document.getElementById("rumus-keliling-jajargenjang");
+
+    if (a > 0 && b > 0) {
+        let keliling = 2 * (a + b);
+        rumuskeliling.textContent = "K = 2 (a + b)";
+        hasilkeliling.innerHTML = "K = 2 ("+ a +" + " + b + ")<br>K = " + keliling;
+    } else {
+        alert('Mohon Isi Form Terlebih Dahulu');
+    }
+}
+
+function resetLuasSegitiga(){
+    document.getElementById("alas-segitiga").value = "";
+    document.getElementById("tinggi-segitiga").value = "";
+    document.getElementById("hasil-Luas").textContent = "";
+    document.getElementById("rumus-luas-segitiga").textContent = "";
 }
 
 function resetKelilingSegitiga() {
-    document.getElementById("panjang-sisi1").value = "";
-    document.getElementById("panjang-sisi2").value = "";
-    document.getElementById("panjang-sisi3").value = "";
-    document.getElementById("hasil-keliling").textContent = "";
+    document.getElementById("sisi1").value = "";
+    document.getElementById("sisi2").value = "";
+    document.getElementById("sisi3").value = "";
+    document.getElementById("hasilKelilingSegitiga").textContent = "";
+    document.getElementById("rumus-keliling-segitiga").textContent = "";
 }
 
-// Jajar Genjang
-function hitungLuasJajarGenjang() {
-    var alasj = document.getElementById("alas-jajar-genjang").value;
-    var tinggij = document.getElementById("tinggi-jajar-genjang").value;
-    var hasilLuasj = document.getElementById("hasil-luas-jajar-genjang");
-
-    if (alasj > 0 && tinggij > 0) {
-        var luasj = alasj * tinggij;
-        hasilLuasj.textContent = "Luas Jajar Genjang adalah: " + luasj;
-    } else {
-        hasilLuasj.textContent = "Masukkan Nilai Panjang Alas dan Tinggi Terlebih Dahulu";
-    }
+function resetFormLuasJajarGenjang(){
+    document.getElementById("alas-jajargenjang").value = "";
+    document.getElementById("tinggi-jajargenjang").value = "";
+    document.getElementById("rumus-luas-jajargenjang").textContent = "";
+    document.getElementById("hasil-luas-jajargenjang").textContent = "";
 }
 
-function hitungKelilingJajarGenjang() {
-    var alasj = parseFloat(document.getElementById("alas-jajar-genjang").value);
-    var sisij = parseFloat(document.getElementById("sisi-jajar-genjang").value);
-    var hasilKelilingj = document.getElementById("hasil-keliling-jajar-genjang");
-
-    if (alasj > 0 && sisij > 0) {
-        var kelilingj = 2 * (alasj + sisij);
-        hasilKelilingj.textContent = "Keliling Jajar Genjang adalah: " + kelilingj;
-    } else {
-        hasilKelilingj.textContent = "Masukkan Nilai Panjang Alas dan Sisi Terlebih Dahulu";
-    }
+function resetFormKelilingJajarGenjang() {
+    document.getElementById("a-jajargenjang").value = "";
+    document.getElementById("b-jajargenjang").value = "";
+    document.getElementById("rumus-keliling-jajargenjang").textContent = "";
+    document.getElementById("hasil-keliling-jajargenjang").textContent = "";
 }
 
-function resetLuasJajarGenjang() {
-    document.getElementById("alas-jajar-genjang").value = "";
-    document.getElementById("tinggi-jajar-genjang").value = "";
-    document.getElementById("hasil-luas-jajar-genjang").textContent = "";
+function tampilkanSegitiga() {
+    // Mengambil elemen dengan class 'main-content-segitiga'
+    const segitigaContent = document.querySelector('.main-content-segitiga');
+    const jajargenjangContent = document.querySelector('.main-content-jajargenjang');
+    // Mengubah display menjadi 'block' untuk menampilkannya
+    segitigaContent.style.display = 'flex';
+    jajargenjangContent.style.display = 'none';
+    
 }
 
-function resetKelilingJajarGenjang() {
-    document.getElementById("alas-jajar-genjang").value = "";
-    document.getElementById("sisi-jajar-genjang").value = "";
-    document.getElementById("hasil-keliling-jajar-genjang").textContent = "";
+function tampilkanJajargenjang() {
+    // Mengambil elemen dengan class 'main-content-jajargenjang'
+    const jajargenjangContent = document.querySelector('.main-content-jajargenjang');
+    const segitigaContent = document.querySelector('.main-content-segitiga');
+    
+    // Mengubah display menjadi 'block' untuk menampilkannya
+    segitigaContent.style.display = 'none';
+    jajargenjangContent.style.display = 'flex';
 }
